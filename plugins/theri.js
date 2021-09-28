d66f0f430c5061945c2cfdc522359a777a35f5cb
gmananthu3 <91454775+gmananthu3@users.noreply.github.com>
2021-09-28 14:45:36 +0530

    Update theri.js

--- plugins/theri.js
+++ plugins/theri.js
@@ -47,7 +47,7 @@ var ldc = ''
 if (Config.LANG == 'AZ') ldc = '*Bağlantı Aşkarlandı!*'
 if (Config.LANG == 'TR') ldc = '*‎Link Tespit Edildi!*'
 if (Config.LANG == 'EN') ldc = '*ith njanum vilikkilla neeyum villikilla* \n *Jack paranju numma cheythu* 😎\n 😅 *arod parayan aar kelkaan* \n *edth purath aakiyathin shesham aan parayunne* 😅😅'
-if (Config.LANG == 'ML') ldc = '*ഇനി vilഇക്കെടാ നിന്റെ തെറി, jack പറഞ്ഞു കിക്ക് ചെയ്യാൻ 😎!*'
+if (Config.LANG == 'ML') ldc = '*ഇനി വിളിക്കെടാ നിന്റെ തെറി, jack പറഞ്ഞു കിക്ക് ചെയ്യാൻ 😎!*'
 if (Config.LANG == 'ID') ldc = '*Tautan Terdeteksi!*'
 if (Config.LANG == 'PT') ldc = '*Link Detectado!*'
 if (Config.LANG == 'RU') ldc = '*Ссылка обнаружена!*'
